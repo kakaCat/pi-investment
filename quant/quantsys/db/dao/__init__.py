30bc184247e87f6d8121187c4c2017cb60b4825a
kakaCat <253834671@qq.com>
2026-05-23 17:05:08 +0800

    feat(dao): add PositionDAO with 5 methods
    - list_positions: get position list with filters
    - get_position: get single position detail
    - update_position: update position fields
    - close_position: close position with reason
    - get_position_summary: get position statistics

--- quant/quantsys/db/dao/__init__.py
+++ quant/quantsys/db/dao/__init__.py
@@ -1,7 +1,9 @@
 """Data Access Object (DAO) layer for quant_agent schema."""
 
 from .base_dao import BaseDAO
+from .position_dao import PositionDAO
 
 __all__ = [
     'BaseDAO',
+    'PositionDAO',
 ]
